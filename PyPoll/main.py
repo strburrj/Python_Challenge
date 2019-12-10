2ebb84492ca52bfedca7547c780c38947c9f344f
strburrj <57110745+strburrj@users.noreply.github.com>
2019-12-09 18:12:23 -0600

    homewk push

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 
 csvpath = os.path.join("/Users/strawberrymeow/Desktop/My_Class_Repo/Python_Challenge/PyPoll/Resources/election_data.csv")
 
-votes = 0
+total_votes = 0
 candidates = []
 number_of_votes = []
 
@@ -11,24 +11,19 @@ with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header=next(csvreader)
     row=next(csvreader)
+    total_votes = total_votes + 1
+
+    row=next(csvreader)
+    total_votes = total_votes + 1
     
 
     for row in csvreader:
-        votes = votes + 1
-
-        if row[2] not in candidates and row[2] not in "Candidate":
-            candidates.append(row[2])
-            
-        elif row[2] in candidates and row [2] not in "Candidate":
-                   
-
-
-
-
+        total_votes = total_votes + 1
 
 print(f"Election Results")
 print(f"------------------")
-print(f"Total Votes:" : str(votes))
-print(f"------------------")
-for count in range(len(candidates)):
-    print(f"{candidates[count]}")
+
+
+    
+
+       
